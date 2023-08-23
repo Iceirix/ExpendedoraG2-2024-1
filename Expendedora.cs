0a6b121f6edfbec7e901193312179b8dad948694
Iceirix <luisfdopapu@gmail.com>
2023-08-23 12:52:02 -0600

    Add Property to Temperatura

--- Expendedora.cs
+++ Expendedora.cs
@@ -8,7 +8,24 @@ namespace ExpendedoraG2_2024_1
         #region Atributos
         private string marca;
         private ushort cantproductos;
+        private byte temperatura;
         private float precio;
+
+
+        #endregion
+
+        #region Propiedades
+        public byte Temperatura
+        { 
+            get => temperatura;
+            set
+            {
+                if(0 < value && value < 25)
+                    temperatura = value;
+                else
+                    temperatura = 20;
+            }
+        }
         #endregion
 
         #region Métodos
@@ -67,8 +84,18 @@ namespace ExpendedoraG2_2024_1
 
         public Expendedora(bool Mantenimiento)
         {
-            if( Mantenimiento == true)
+            Temperatura = 20;
+            if (Mantenimiento == true)
+            {
                 Console.WriteLine("Entrando en modo mantenimiento");
+                Console.WriteLine("Cambiando temperatura");
+                LimpiarDisplay();
+                for (int i = 0; i < 20; i++)
+                {
+                    Temperatura++;
+                }
+                Console.WriteLine("Mostrando temperatura {0}[°C]", Temperatura);
+            }
         }
         #endregion
     }
